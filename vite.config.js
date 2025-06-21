bd46780ac32ccdf5762800d5d0188a7c8b2f39f7
manasvirana <manasvirana92@gmail.com>
2025-06-21 19:21:07 +0530

    Ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: 'main-app-or-music-library', 
-      remotes: {
-        
-        music_library: 'http://localhost:4174/assets/remoteEntry.js'
-      },
-      exposes: {
-        
-        './MusicLibrary': './src/MusicLibrary.jsx'
-      },
-      shared: ['react', 'react-dom']
-    })
-  ],
-  build: {
-    target: 'esnext'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: 'main_app',
+        remotes: {
+          music_library: `${env.VITE_MUSIC_LIBRARY_URL}` 
+        },
+        shared: ['react', 'react-dom']
+      })
+    ],
+    build: {
+      target: 'esnext'
+    }
   }
 })
